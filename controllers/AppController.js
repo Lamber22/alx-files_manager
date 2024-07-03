3f2ab889149aed6239e960b5b5c3c18f5fd0febf
Joe Egboka <joeegboka@gmail.com>
2024-07-03 11:20:20 +0100

    modify server.js and AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,11 +2,11 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 export const getStatus = (req, res) => {
-  res.status(200).send({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
 };
 
 export const getStats = async (req, res) => {
   const users = await dbClient.nbUsers();
   const files = await dbClient.nbFiles();
-  res.status(200).send({ users, files });
+  res.status(200).json({ users, files });
 };
