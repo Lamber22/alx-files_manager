a91af7bd7e9ac6308aa3bfbc88c0289fbaa5d2c3
Joe Egboka <joeegboka@gmail.com>
2024-07-05 12:37:27 +0100

    install body-parser to enable request body to be read in th server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import router from './routes/index';
 
 const app = express();
 const port = process.env.PORT || '5000';
 
+app.use(bodyParser.json());
 app.use(router);
 
 app.listen(port, () => {
